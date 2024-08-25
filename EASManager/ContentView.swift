b38353a33c40435392eb4e78e62ee8f266509d8b
Henry Paulino <corasan.dev@gmail.com>
2024-08-24 21:52:19 -0400

    Initial Commit

---
+++ EASManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EASManager
+//
+//  Created by Henry on 8/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EASManager/EASManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EASManagerApp.swift
+//  EASManager
+//
+//  Created by Henry on 8/24/24.
+//
+
+import SwiftUI
+
+@main
+struct EASManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
