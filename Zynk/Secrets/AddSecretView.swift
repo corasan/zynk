1b5b6740304e5dd6b99134cd99a7915cd5d76fc3
Henry Paulino <corasan.dev@gmail.com>
2024-08-28 20:25:01 -0400

    add ability to add multiple envs in a single save

--- Zynk/Secrets/AddSecretView.swift
+++ Zynk/Secrets/AddSecretView.swift
@@ -25,28 +25,102 @@ struct AddSecretButton: View {
     }
 }
 
+struct NewSecret: Identifiable, Equatable {
+    let id: UUID
+    let variable: String
+    let value: String
+}
+
+class SecretsViewModel: ObservableObject {
+    @Published var secrets: [NewSecret] = []
+    
+    init() {
+        // Initialize with some sample data
+        secrets = [
+            NewSecret(id: UUID(), variable: "", value: ""),
+        ]
+    }
+    
+    func updateSecret(id: UUID, variable: String, value: String) {
+        if let index = secrets.firstIndex(where: { $0.id == id }) {
+            secrets[index] = NewSecret(id: id, variable: variable, value: value)
+        }
+    }
+    
+    func addSecret() {
+        let newSecret = NewSecret(id: UUID(), variable: "", value: "")
+        secrets.append(newSecret)
+    }
+    
+    func removeSecret(_ secret: NewSecret) {
+        let index = secrets.firstIndex(of: secret)
+        if let index = index {
+            secrets.remove(at: index)
+        }
+    }
+}
+
 struct AddSecretView: View {
     @Binding var isPresented: Bool
     @State private var variable = ""
     @State private var value = ""
     @EnvironmentObject var secretsManager: SecretsManager
+    @StateObject private var viewModel = SecretsViewModel()
 
     var body: some View {
         VStack(alignment: .leading) {
             Text("Add Secret")
                 .font(.title)
-            HStack(spacing: 8) {
-                TextField("Variable", text: $variable)
-                TextField("Value", text: $value)
+            LazyVStack {
+                ForEach($viewModel.secrets) { $secret in
+                    HStack {
+                        TextField("Variable", text: Binding(
+                            get: { secret.variable },
+                            set: { viewModel.updateSecret(id: secret.id, variable: $0, value: secret.value) }
+                        ))
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        
+                        TextField("Value", text: Binding(
+                            get: { secret.value },
+                            set: { viewModel.updateSecret(id: secret.id, variable: secret.variable, value: $0) }
+                        ))
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        if $viewModel.secrets.count > 1 {
+                            Button(action: { viewModel.removeSecret(secret) }) {
+                                Image(systemName: "minus")
+                            }
+                        }
+                    }
+                }
             }
+            .padding(.vertical, 6)
+            .padding(.horizontal, 4)
+           
             HStack {
-                Button("Cancel",role: .cancel, action: {
+                Button(role: .cancel, action: {
                     isPresented.toggle()
-                })
+                }) {
+                    Text("Cancel")
+                        .padding(4)
+                }
                 Spacer()
-                Button("Save", action: saveSecret)
+                HStack {
+                    Button(action: { viewModel.addSecret()} ) {
+                        HStack {
+                            Image(systemName: "plus")
+                            Text("Add more")
+                        }
+                        .padding(4)
+                    }
+                    .buttonStyle(.bordered)
+                    Button(action: saveSecret) {
+                        Text("Save")
+                            .padding(4)
+                    }
                     .buttonStyle(.borderedProminent)
-                    .disabled(variable.isEmpty || value.isEmpty)
+                    .disabled(!isValid())
+                }
+                
             }
             .padding(.top, 16)
         }
@@ -54,8 +128,19 @@ struct AddSecretView: View {
     }
     
     func saveSecret() {
-        secretsManager.addItem(key: variable, value: value)
+        for secret in viewModel.secrets {
+            secretsManager.addItem(key: secret.variable, value: secret.value)
+        }
         isPresented.toggle()
+    }
+    
+    func isValid() -> Bool {
+        for secret in viewModel.secrets {
+            if secret.variable.isEmpty || secret.value.isEmpty {
+                return false
+            }
+        }
+        return true
     }
 }
 
