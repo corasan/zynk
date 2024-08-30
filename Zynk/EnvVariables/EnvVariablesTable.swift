7df63afcd464e1ba0b0c424eab9cbebd97b78f17
Henry Paulino <corasan.dev@gmail.com>
2024-08-29 21:49:11 -0400

    Feature: row context menu (#6)

--- Zynk/EnvVariables/EnvVariablesTable.swift
+++ Zynk/EnvVariables/EnvVariablesTable.swift
@@ -10,23 +10,52 @@ import SwiftUI
 struct EnvVariablesTable: View {
     @State private var showAddSheet = false
     @State private var selectedRows: Set<Variable.ID> = []
+    @State private var selectedRow: Variable.ID?
     @State private var sortOrder = [KeyPathComparator(\Variable.variable)]
-    @State private var showPopOver = false
+    @State private var showEditSheet = false
+    @State private var editVariable: Variable?
     @EnvironmentObject var envsModel: EnvVariablesModel
     
     var body: some View {
         VStack(spacing: 0) {
             Dropzone {
-                Table(envsModel.variables, selection: $selectedRows) {
+                Table(of: Variable.self, selection: $selectedRows) {
                     TableColumn("Variable", value: \.variable)
                     TableColumn("Value", value: \.value)
+                } rows: {
+                    ForEach(envsModel.variables) { variable in
+                        TableRow(variable)
+                            .contextMenu {
+                                Button(action: {
+                                    editVariable = variable
+                                    print("showEditSheet \(showEditSheet)")
+                                }) {
+                                    HStack {
+                                        Image(systemName: "pencil")
+                                        Text("Edit")
+                                    }
+                                }
+                                Button(role: .destructive, action: { envsModel.removeItem(id: variable.id) }) {
+                                    HStack {
+                                        Image(systemName: "trash")
+                                        Text("Delete")
+                                    }
+                                }
+                            }
+                    }
                 }
+            }
+            .sheet(item: $editVariable) { variable in
+                EditEnvVariableView(item: variable, isPresented: Binding(
+                    get: { editVariable != nil },
+                    set: { if !$0 { editVariable = nil } }
+                ))
             }
                         
             HStack {
                 AddEnvVariableButton()
 
-                Button(action: removeItem) {
+                Button(action: { envsModel.removeSelectedItems(items: selectedRows) }) {
                     Label("Remove", systemImage: "minus")
                         .foregroundStyle(.foreground)
                         .fontWeight(.medium)
@@ -50,20 +79,6 @@ struct EnvVariablesTable: View {
             if newVal {
                 envsModel.reload()
                 envsModel.didUpload = false
-            }
-        }
-    }
-    
-    func addItem() {
-        showAddSheet.toggle()
-    }
-    
-    func removeItem() {
-        if !envsModel.variables.isEmpty {
-            for row in selectedRows {
-                if let index = envsModel.variables.firstIndex(where: { $0.id == row.self }) {
-                    envsModel.removeItem(at: IndexSet(integer: index))
-                }
             }
         }
     }
